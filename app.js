af5a16bba831aa0a043a884193c4bd96f955255b
Sam <re711kimo@gmail.com>
2020-06-22 22:07:50 +0800

    add detail page

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const app = express()
 const PORT = 3000
 
 const db = require('./models')
+
 const Todo = db.Todo
 const User = db.User
 
@@ -23,6 +24,13 @@ app.get('/', (req, res) => {
   })
     .then((todos) => { return res.render('index', { todos: todos }) })
     .catch((error) => { return res.status(422).json(error) })
+})
+
+app.get('/todos/:id', (req, res) => {
+  const id = req.params.id
+  return Todo.findByPk(id)
+    .then(todo => res.render('detail', { todo: todo.toJSON() }))
+    .catch(error => console.log(error))
 })
 
 app.get('/users/login', (req, res) => {
